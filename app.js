2f10e2de5831e4ae68ee0073fd3a1d175cfc0fcd
Hebrews Gebremariam <83418575+hebree123@users.noreply.github.com>
2021-06-27 17:25:38 -0400

    list functionality and trashcan added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const todoInput = document.querySelector('.todo_input');
 const todoButton = document.querySelector('.todo_button');
 const todoList = document.querySelector('.todo_list');
+const todoDelete = document.querySelector('.todo_delete');
+
 
 todoButton.addEventListener("click", addTodo)
 
 function addTodo(event) {
     event.preventDefault();
+    todoInput.value.trim();
     //todo DIV
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
@@ -14,6 +17,31 @@ function addTodo(event) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo_item');
     todoDiv.appendChild(newTodo);
+
     if(todoInput.value === ""){
-        return null }
+        return false;
+    }
+    
+    if(todoList.childElementCount > 6) {
+        return null;
+    }
+
+    todoList.appendChild(todoDiv);
+
+    todoInput.value = "";
+
+    //task checkbox
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="far fa-circle"></i>';
+    completedButton.classList.add('complete_btn')
+    todoDiv.appendChild(completedButton);
+
+}
+
+todoDelete.addEventListener('click', clearList)
+
+function clearList() {
+    if (todoList.childElementCount >= 1) {
+        todoList.innerHTML = ""
+    }
 }

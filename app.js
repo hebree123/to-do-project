553036e68d2726d5b2d0312b3b4462ce75433b37
Hebrews Gebremariam <83418575+hebree123@users.noreply.github.com>
2021-07-02 19:34:13 -0400

    animations, responsiveness

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const todoButton = document.querySelector('.todo_button');
 const todoList = document.querySelector('.todo_list');
 const todoDelete = document.querySelector('.todo_delete');
 
-
+gsap.from('header', {opacity:0 , duration:1, y:-200});
+gsap.from('main', {opacity:0 , duration:1, x:-200});
+gsap.from('form', {opacity:0 , duration:1, x:200});
+gsap.from('footer', {opacity:0 , scale: 0.3, duration:1, y:200, delay: 0.3});
 
 todoButton.addEventListener("click", addTodo)
 
@@ -27,6 +30,8 @@ function addTodo(event) {
         return null;
     }
 
+    gsap.fromTo('.todo_button', {scale:0.5,duration:0.3}, {scale:1});
+
     todoList.appendChild(todoDiv);
 
     todoInput.value = "";
@@ -48,33 +53,34 @@ function addTodo(event) {
     todoDiv.appendChild(deleteTodo);
 
     
-
+    //fill checkbox 
     completedButton.addEventListener('click', finishTask);
-
     function finishTask() {
         completedButtonFilled.style.display = ('block');
         todoDiv.classList.toggle('complete_todo');
         deleteTodo.style.display = ('block');
     } 
     
+    //unfill checkbox
     completedButtonFilled.addEventListener('click', unfinishTask);
-
     function unfinishTask() {
         completedButtonFilled.style.display = ('none');
         todoDiv.classList.toggle('complete_todo');
         deleteTodo.style.display = ('none');
     };
 
+    //delete todos
     deleteTodo.addEventListener('click', function(){
         this.parentElement.remove();
     });
 
 }
 
+//clear todolist
 todoDelete.addEventListener('click', clearList);
-
 function clearList() {
     if (todoList.childElementCount >= 1) {
+        gsap.fromTo('.todo_delete', {scale:1.5,duration:0.5},{scale:1});
         todoList.innerHTML = "";
     }
 }
